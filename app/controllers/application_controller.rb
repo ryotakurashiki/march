2c81f230ff345b95ccf587ed13e36e9acf9d8900
ryotakurashiki <kura.hutak0bu@gmail.com>
2016-09-12 10:37:53 +0900

    deviseメール認証後に自動ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_confirmation_path_for(resource_name, resource)
+    root_path
+  end
+
   def after_sign_in_path_for(resource)
     admin_signed_in? ? admin_deactive_concerts_path : root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   devise_for :admins, only: [:sessions]
   namespace :admin do
     resources :deactive_concerts
