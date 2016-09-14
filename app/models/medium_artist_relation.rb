2bc275e64030fb135a427b6d2d662583edbf296f
ryotakurashiki <kura.hutak0bu@gmail.com>
2016-09-14 12:09:08 +0900

    artist_relationsのスクレイピングでartist_idも入れる

--- app/models/medium_artist_relation.rb
+++ app/models/medium_artist_relation.rb
@@ -5,8 +5,12 @@ class MediumArtistRelation < ApplicationRecord
   after_create :create_crawl_status
   has_one :crawl_status, dependent: :destroy
 
-  scope :eplus, -> { where(id: 1) }
-  scope :camp, -> { where(id: 2) }
+  scope :eplus, -> { where(medium_id: 1) }
+  scope :camp, -> { where(medium_id: 2) }
+  scope :crawlable, -> (period) {
+    joins(:crawl_status).
+    where("crawl_statuses.crawled_on IS NULL OR crawl_statuses.crawled_on < ?", period.days.ago)
+  }
 
   def eplus_url
   	"https://eplus.jp/ath/word/" + medium_artist_id
