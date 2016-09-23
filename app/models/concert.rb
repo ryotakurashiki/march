b0984a39bc6ec7779ae575e6864eeabaf2946120
ryotakurashiki <kura.hutak0bu@gmail.com>
2016-09-24 02:19:29 +0900

    出演アーティストにお気に入りのアーティストから表示する

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -15,9 +15,19 @@ class Concert < ApplicationRecord
     date < days.days.ago.to_date
   end
 
+  def appearance_artists_filtered(user)
+    #artists.includes(:favorite_artists).where(favorite_artists: {user_id: user.id}).order("favorite_artists.artist_id")
+    artists.find_by_sql(
+      "SELECT artists.*, favorite_artists.id from artists LEFT OUTER JOIN favorite_artists
+       ON favorite_artists.artist_id = artists.id AND favorite_artists.user_id = #{user.id}
+       INNER JOIN appearance_artists ON artists.id = appearance_artists.artist_id
+       WHERE appearance_artists.attachable_id = #{self.id} AND appearance_artists.attachable_type = 'Concert'
+       ORDER BY favorite_artists.id DESC"
+    )
+  end
+
   def self.find_or_create_by(params)
     #self.find_by(title: params[:title], date: params[:date], place: params[:place]) || self.create(params)
-
     if concert = self.find_by(date: params[:date], place: params[:place])
       # e+IDが変わっただけなのに更新されるのを防ぐ、一方で日付と場所だけだと同日同会場の更新ができない
       unless concert.eplus_id == params[:eplus_id]
