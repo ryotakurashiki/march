fed0c872493b7bb9d49dfcf8249569c8e7c46e8e
ryotakurashiki <kura.hutak0bu@gmail.com>
2016-10-10 23:50:26 +0900

    既にお気に入り登録済みのアーティストは関連アーティストで出さない

--- app/controllers/user/favorite_artists_controller.rb
+++ app/controllers/user/favorite_artists_controller.rb
@@ -15,10 +15,11 @@ class User::FavoriteArtistsController < User::UserApplicationController
       @favorite_artist = FavoriteArtist.new(artist_id: params[:artist_id], user_id: current_user.id)
       old_recommended_artist_ids = current_user.recommended_artist_ids
       if @favorite_artist.save
-        new_recommended_artist_ids = @favorite_artist.artist.related_artist_ids - old_recommended_artist_ids
-        @related_artists = Artist.where(id: new_recommended_artist_ids, category: nil)
         @favorite_counts = current_user.favorite_artists.size
         @favorite_artist_ids = current_user.favorite_artists.pluck(:artist_id)
+        new_recommended_artist_ids =
+          @favorite_artist.artist.related_artist_ids - old_recommended_artist_ids - @favorite_artist_ids
+        @related_artists = Artist.where(id: new_recommended_artist_ids, category: nil)
       else
       end
     end
