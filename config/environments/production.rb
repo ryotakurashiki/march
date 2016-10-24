baf4a2a06428399be6a275000a26391f8bb7ffd5
ryotakurashiki <kura.hutak0bu@gmail.com>
2016-10-24 12:56:39 +0900

    production環境のhost変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "otolog_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "otolog.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "otolog.net" }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
